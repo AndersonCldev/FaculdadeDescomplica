3f337953ce5e5007c5621ea8bfbf1dbcc0a0aa92
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-03-04 11:45:34 -0300

    Update pensareResponderFaculdade.java
    Add função de soma de todos os valores que estão condidos no ArrayList numeros.

--- pensareResponderFaculdade.java
+++ pensareResponderFaculdade.java
@@ -2,23 +2,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class pensareResponderFaculdade {
-	public static void main(String args[]) {
-		
-		ArrayList<Integer> numeros = new ArrayList<>();
-		Scanner ler = new Scanner(System.in);
-		System.out.print("Digite 1 número: ");
-		
-		for(int i = 0; i < 5; i++) {
-			
-			int numeros1 = ler.nextInt();
-			numeros.add(numeros1);
-			
-		}
-		System.out.println("Os números digitados foram: ");
-		for(int i = 0; i < numeros.size(); i++) {
-			int numero = numeros.get(i);
-			System.out.println(numero);
-		}
-		
-	}
+    public static void main(String args[]) {
+        
+        ArrayList<Integer> numeros = new ArrayList<>();
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite 5 números: ");
+
+        for (int i = 0; i < 5; i++) {
+            int numero = ler.nextInt();
+            numeros.add(numero);
+        }
+
+        System.out.println("Os números digitados foram: ");
+        for (int i = 0; i < numeros.size(); i++) {
+            int numero = numeros.get(i);
+            System.out.println(numero);
+        }
+
+        System.out.println("Agora vamos somar todos os números informados no ArrayList ");
+        int soma = 0;
+        for (int i = 0; i < numeros.size(); i++) {
+            soma += numeros.get(i);
+        }
+
+        System.out.println("TOTAL: " + soma);
+    }
 }
